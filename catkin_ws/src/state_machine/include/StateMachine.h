ce0fc602ad99c9479a0b98ffdb284359bbd33bb6
Yann Briançon <briyan@sfr.fr>
2016-05-03 19:23:49 +0200

    Update StateMachine.h

--- catkin_ws/src/state_machine/include/StateMachine.h
+++ catkin_ws/src/state_machine/include/StateMachine.h
@@ -1,24 +1,24 @@
 #ifndef STATEMACHINE_H_INCLUDED
 #define STATEMACHINE_H_INCLUDED
 
-#include<iostream>
-#include<vector>
-#include<string>
+//#include<iostream>
+//#include<vector>
+//#include<string>
 
 class State{
 private:
     std::vector<std::string> names; //le vecteur stockant le nom des transitions
-    std::vector<bool> conditions; //vecteur de booléeens pour passerd'un état à un autre
-    std::vector<State*> followState; //les liaisons avec les autres états
+    std::vector<bool> conditions; //vecteur de boolÃ©eens pour passerd'un Ã©tat Ã  un autre
+    std::vector<State*> followState; //les liaisons avec les autres Ã©tats
     int l; //longueur des vecteurs
     State(const State &); //verrouillage du constructeur de recopie
-    State & operator = (const State &); //verrouillage de l'opérateur d'affectation
+    State & operator = (const State &); //verrouillage de l'opÃ©rateur d'affectation
 public:
-    State(void(*ptfoncaddress)(State*)); //Constructeur.il prend en argument l'adresse d'une fonction définie par l'utilisateur
+    State(void(*ptfoncaddress)(State*)); //Constructeur.il prend en argument l'adresse d'une fonction dÃ©finie par l'utilisateur
     ~State(void); //Destructeur
-    void(*ptfonc)(State*); //Un pointeur sur une fonction définie ultérieurement par l'utilisateur
-    void addTransition(std::string name,State* pt); //méthode de construction des liaisons
-    void setTransition(std::string name,bool b); //méthode pour activer une transition (mettre à "true" le booléen)
+    void(*ptfonc)(State*); //Un pointeur sur une fonction dÃ©finie ultÃ©rieurement par l'utilisateur
+    void addTransition(std::string name,State* pt); //mÃ©thode de construction des liaisons
+    void setTransition(std::string name,bool b); //mÃ©thode pour activer une transition (mettre Ã  "true" le boolÃ©en)
 
 friend class SM;
 
@@ -26,14 +26,14 @@ friend class SM;
 
 class SM{
 private:
-    State* CurrentStatePointer; //Un pointeur sur l'état courant
+    State* CurrentStatePointer; //Un pointeur sur l'Ã©tat courant
     SM(const SM &); //verrouillage du constructeur de recopie
-    SM & operator = (const SM &); //verrouillage de l'opérateur d'affectation
+    SM & operator = (const SM &); //verrouillage de l'opÃ©rateur d'affectation
 
 public:
-    SM(State* init); //Constructeur initialisant la SM sur un état donné
+    SM(State* init); //Constructeur initialisant la SM sur un Ã©tat donnÃ©
     ~SM(void); //Destructeur
-    void run(); //Il suffit d'appeler cette fonction après construction de la SM, dans une boucle, et tout fonctionne tout seul !
+    void run(); //Il suffit d'appeler cette fonction aprÃ¨s construction de la SM, dans une boucle, et tout fonctionne tout seul !
 
 };
 
